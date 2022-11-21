4257d7ac7929ecb3c7321e3b755e1c54042d7165
vmpoojary <vmpoojary@gmail.com>
2022-11-21 22:41:57 +0530

    End of section 10

---
+++ client/src/app/_services/busy.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BusyService {
+
+  busyRequestCount=0;
+
+  constructor(private spinnerServcie:NgxSpinnerService) { }
+
+  busy()
+  {
+    this.busyRequestCount++;
+    this. spinnerServcie.show(undefined,{
+      type:'line-space-party',
+      bdColor:'rgba(255,255,255,0)',
+      color:'#333333'
+    })
+  }
+
+  idle()
+  {
+    this.busyRequestCount--;
+    if(this.busyRequestCount <=0)
+    this.busyRequestCount=0;
+    this.spinnerServcie.hide(); 
+  }
+
+}
--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/Member';
 
@@ -11,16 +11,36 @@ import { Member } from '../_models/Member';
 })
 export class MembersService {
   baseUrl=environment.apiUrl;
+  members:Member[]=[];   
   constructor(private http:HttpClient) { }
 
   getMembers()
   {
-    return this.http.get<Member[]>(this.baseUrl+'users')
+    if(this,this.members.length>0) return of(this.members);
+    return this.http.get<Member[]>(this.baseUrl+'users').pipe(
+        map(members=>{
+          this.members=members;
+          return members;
+        })
+    )
   }
 
   getMember(userName:string)
   {
+    const member=this.members.find(x=>x.userName===userName)
+    if(member!==undefined) return of(member)
     return this.http.get<Member>(this.baseUrl+'users/'+userName)
+  }
+
+  updateMember(member:Member)
+  {
+    return this.http.put(this.baseUrl + 'users/', member)
+    .pipe(
+      map(()=>{
+        const index=this.members.indexOf(member);
+        this.members[index]=member; 
+      }))
+    ;
   }
 
 }
